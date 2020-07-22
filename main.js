500b212d9f5190e6e0d326bc5df54a7fd46c95c8
dudziakpudziak <lukaszdudziak777@gmail.com>
2020-07-22 21:02:13 +0200

    final_200722 (working countdown)

--- main.js
+++ main.js
@@ -6,26 +6,117 @@ const alarmSet = document.querySelector('.alarmSet');
 const alarmReset = document.querySelector('.alarmReset');
 const alarmSetting = document.querySelector('.alarmSetting');
 
+//if you don't declare this interval variable, you won't clear it in any other function than origin one
+let alarmInterval;
+
+//current time function to implement in simple clock
 const currentTime = () => {
     let now = new Date()
-    currentHour.textContent = `${now.getHours()}:${now.getMinutes()}`;
+    if (now.getMinutes() < 10) {
+        //if you don't do this, clock won't show minutes in wanted format (hh:mm)
+        currentHour.textContent = `${now.getHours()}:0${now.getMinutes()}`;
+    } else {
+        currentHour.textContent = `${now.getHours()}:${now.getMinutes()}`;
+    }
 }
-
+//simple clock 
 const clock = () => {
     setInterval(currentTime, 1000);
 }
-
+//checking is it time to alarm
 const isItTheTime = () => {
     if (currentHour.textContent == alarmSetting.textContent) {
         alarmSection.classList.add('ring');
-        alert('Ring ring motherfucker')
+        //if you don't clear this interval, alert gonna block any action until "alarm minute" is gonna end
+        clearInterval(alarmInterval);
+        return alert('Ring ring motherfucker')
     }
 }
+//setting alarm time
 const alarmFunction = () => {
     if (!alarmTime.value) alert(`You didn't set the time`)
     alarmSetting.textContent = alarmTime.value;
-    setInterval(isItTheTime, 1000);
+    alarmInterval = setInterval(isItTheTime(), 1000);
+}
+// alarm reset function
+const alarmClear = () => {
+    alarmTime.value = null;
+    alarmSetting.textContent = "--:--";
 }
 
 clock()
 alarmSet.addEventListener('click', alarmFunction);
+alarmReset.addEventListener('click', alarmClear);
+
+
+// COUNTDOWNS SECTION 
+
+//hh-mm-ss countdown
+countdownSection = document.querySelector('.countdown');
+countdownHour = document.querySelector('.countdownHour');
+countdownMinute = document.querySelector('.countdownMinute');
+countdownSeconds = document.querySelector('.countdownSeconds');
+countdownSet = document.querySelector('.countdownSet');
+countdownStart = document.querySelector('.countdownStart');
+countdownReset = document.querySelector('.countdownReset');
+let countdownInterval;
+
+//checking if input is in hh-mm-ss format
+const checkMinutesSeconds = (time) => {
+    if (time >= 60) return alert(`Minute's and second's must be in range 00-59`);
+}
+//countdown function
+const countdown = () => {
+
+    //special effect for last 10 seconds 
+    if (countdownSeconds.value < 10 && countdownHour.value == 0 && countdownMinute.value == 0) {
+        countdownSection.classList.toggle('ring');
+    }
+
+    //countdown function core, decrementing seconds and changing sec/min/hour input when it hits 0 + clearing whole thing, when all of those inputs hit 0
+    countdownSeconds.value--
+    if (countdownSeconds.value < 0) {
+        countdownMinute.value--;
+        countdownSeconds.value = 59;
+        if (countdownMinute.value < 0) {
+            countdownHour.value--;
+            countdownMinute.value = 59;
+            if (countdownHour.value < 0) {
+                clearInterval(countdownInterval);
+                //reset inputs after countdown ends
+                countdownHour.value = '00';
+                countdownMinute.value = '00';
+                countdownSeconds.value = '00';
+                alert('End of countdown!');
+            }
+        }
+    }
+}
+
+//setting interval for counting down
+const startCountdown = () => {
+    countdownInterval = setInterval(countdown, 1000);
+}
+
+//setting function
+const setCountdown = () => {
+    checkMinutesSeconds(countdownMinute.value);
+    checkMinutesSeconds(countdownSeconds.value);
+    alert('You can start countdown!');
+
+    //if all inputs are corectly set, you can use start button
+    countdownStart.addEventListener('click', startCountdown);
+}
+
+//reset countdown button
+const resetCountdown = () => {
+    clearInterval(countdownInterval);
+    countdownHour.value = '00';
+    countdownMinute.value = '00';
+    countdownSeconds.value = '00';
+}
+
+countdownSet.addEventListener('click', setCountdown);
+countdownReset.addEventListener('click', resetCountdown);
+
+//countdown To date section
